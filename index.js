95051697840f51bbd2065cbb75d9e308b8aa6c02
Neil Bennett <neil.bennett876@Neils-MacBook-Air.local>
2022-04-29 11:16:38 -0400

    created first restaurant

--- index.js
+++ index.js
@@ -1,8 +1,21 @@
 const admin = require("firebase-admin");
 
-const serviceAccount = require("path/to/serviceAccountKey.json");
+const serviceAccount = require('./credentials.json');
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount)
 });
-const
+
+const db = admin.firestore();
+
+const restaurant  = {
+    name: "Mister 01",
+    address: "555 N Federal HWY",
+    cusisine: "pizza",
+    rating: 4.9,
+    phone: "(786) 677-2903"
+}
+
+db.collection('restaurants').add(restaurant)
+.then(doc => console.log("Created restaurant", doc.id))
+.catch(err => console.error(err))
